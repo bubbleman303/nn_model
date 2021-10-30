529b1fa3c59277bdd924825c61a4b597b8cf3a88
keizaburo.takashiba@airitech.co.jp <keizaburo.takashiba@airitech.co.jp>
2021-10-30 20:26:05 +0900

    hello

--- src/models/neural_net.py
+++ src/models/neural_net.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-from src.models import layers
+from src.models import layers, optimizers
 from src.conf import config
 import matplotlib.pyplot as plt
 
@@ -8,14 +8,16 @@ import matplotlib.pyplot as plt
 class NeuralNetWork:
     def __init__(self, lr=0.01, batch_size=300, input_size=None, hidden_size=None, output_size=None, depth=0,
                  weight_init_std=0.01,
-                 load_nn_name=None):
+                 load_nn_name=None, optimizer=optimizers.SGD):
         self.layers = []
         self.lr = lr
         self.batch_size = batch_size
         self.last_layer = None
         self.loss_list = []
+        self.optimizer = None
         if load_nn_name:
             self.load_nn(load_nn_name)
+            self.init_optimizer(optimizer)
             return
         self.weight_init_std = weight_init_std
         self.layers.append(layers.AffineLayer(self.weight_init(input_size, hidden_size), np.zeros(hidden_size)))
@@ -29,6 +31,10 @@ class NeuralNetWork:
         # self.layers.append(layers.BatchNorm())
         self.layers.append(layers.SigmoidLayer())
         self.last_layer = layers.MeanSquareLoss()
+        self.init_optimizer(optimizer)
+
+    def init_optimizer(self, optimizer):
+        self.optimizer = optimizer([layer for layer in self.layers if type(layer) == layers.AffineLayer])
 
     @staticmethod
     def weight_init(i, o):
@@ -60,10 +66,7 @@ class NeuralNetWork:
         d_out = self.last_layer.backward(d_out)
         for layer in self.layers[::-1]:
             d_out = layer.backward(d_out)
-        for layer in self.layers:
-            if type(layer) == layers.AffineLayer:
-                layer.w -= layer.dw * self.lr
-                layer.b -= layer.db * self.lr
+        self.optimizer.update()
 
     def save_nn(self, name):
         param_dic = {"lr": self.lr, "batch_size": self.batch_size, "net": []}
